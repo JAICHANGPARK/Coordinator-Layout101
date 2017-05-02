0a0175ddd428c47415c5aeee84b23baaff6393a8
Dreamwalker <aristojeff@gmail.com>
2017-05-02 17:23:51 +0900

    coooooadd
    add

---
+++ app/src/main/java/com/dreamwalker/collapsinglayout101/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dreamwalker.collapsinglayout101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
